811764351af30eecd211db3029b755add65d7912
Kevin McKernan <kevin@mckernan.in>
2016-10-13 10:52:05 -0600

    design complete w/ responsive styles

---
+++ themes/e3challenge/functions.php
@@ -0,0 +1,97 @@
+<?php
+
+class E3_Challenge {
+
+	function __construct() {
+
+		add_filter( 'upload_mimes', 		array( $this, 'mime_types' ) );
+		add_action( 'cmb2_init',			array( $this, 'event_metaboxes' ) );
+		add_action( 'after_setup_theme',	array( $this, 'custom_images' ) );
+
+		$this->cpt_registration();
+
+	}
+
+	public function cpt_registration() {
+		include_once( 'inc/class-cpt.php' );
+
+		if ( class_exists( 'CPT' ) ) {
+
+			$events = new CPT( array(
+				'post_type_name' => 'event',
+				'singular'       => 'Event',
+				'plural'         => 'Events',
+				'slug'           => 'event',
+				),
+				array(
+			    'supports' => array( 'title', 'editor', 'thumbnail' ),
+			));
+		}
+	}
+
+	/**
+	 * Add custom allowed media upload file types.
+	 */
+
+	public function mime_types( $mimes ) {
+		$mimes['svg'] = 'image/svg+xml';
+
+		return $mimes;
+	}
+
+	static function get_field( $field_name = null, $id = null ) {
+		if ( null === $id ) {
+			$id = get_the_ID();
+		}
+		if ( false === strpos( $field_name, '_e3_' ) ) {
+			$field_name = '_e3_' . $field_name;
+		}
+		$field = get_post_meta( $id, $field_name, true );
+		return '' !== $field ? $field : false;
+	}
+
+	/**
+	 * Define the metabox and field configurations.
+	 */
+	public function event_metaboxes() {
+
+		// Start with an underscore to hide fields from custom fields list
+		$prefix = '_e3_';
+
+		/**
+		 * Initiate the metabox
+		 */
+		$cmb = new_cmb2_box( array(
+			'id'            => 'event_fields',
+			'title'         => __( 'Event Fields', 'e3challenge' ),
+			'object_types'  => array( 'event' ), // Post type
+			'context'       => 'normal',
+			'priority'      => 'high',
+			'show_names'    => true, // Show field names on the left
+		));
+
+		$cmb->add_field( array(
+			'name' => __( 'Date', 'cmb2' ),
+			'id'   => $prefix . 'date',
+			'type' => 'text_date_timestamp',
+		));
+
+		$cmb->add_field( array(
+			'name' => __( 'Start Time', 'cmb2' ),
+			'id'   => $prefix . 'start_time',
+			'type' => 'text_time',
+		));
+
+		$cmb->add_field( array(
+			'name' => __( 'End Time', 'cmb2' ),
+			'id'   => $prefix . 'end_time',
+			'type' => 'text_time',
+		));
+	}
+
+	public function custom_images() {
+		add_image_size( 'event-thumb', 385, 200, [ 'left', 'top' ] );
+	}
+}
+
+new E3_Challenge();
---
+++ themes/e3challenge/template-home.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * @package WordPress
+ * @subpackage E3_Challenge
+ * @since E3_Challenge 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area fullwidth">
+	<main id="main" class="site-main" role="main">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+		?>
+
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+
+			<div class="entry-content">
+				<div class="hero">
+					<div class="background"></div>
+					<?php
+					the_content();
+					?>
+				</div>
+
+				<div class="featured-event">
+					<img src="http://localhost:3000/wp-content/uploads/2016/10/e3_challenge_hero.jpg" />
+					<div class="content">
+						<h3>September 29, 2016</h3>
+						<h2>Real Rock Star Awards Gala</h2>
+						<p>
+							Back for its fourth year, the 2016 Real Rock Star Awards #RRSA will bring together local and national musicians to honor Youth on Record’s most resilient and talented students in a one-of-a-kind red carpet event for a new generation. The evening includes amazing music and performances, dinner, drinks, and the unique opportunity to be part of this fabulous event!
+						</p>
+						<a class="button" href="#">Learn More</a>
+					</div>
+				</div>
+
+				<div class="upcoming-events">
+					<h2 class="section-header">Upcoming Events</h2>
+					<div class="events">
+						<?php
+							$args = [
+								'post_type'      => 'event',
+								'posts_per_page' => -1,
+								'meta_key'       => '_e3_date',
+								'orderby'        => 'meta_value_num',
+								'order'          => 'ASC',
+							];
+
+							$query = new WP_Query( $args );
+							while ( $query->have_posts() ) {
+								$query->the_post();
+								$date = E3_Challenge::get_field( 'date' );
+								?>
+								<div class="event">
+									<div class="img-wrap">
+										<?php the_post_thumbnail( 'event-thumb' ); ?>
+									</div>
+
+									<div class="date">
+										<span class="month"><?php echo esc_html( date( 'M', $date ) ); ?></span>
+										<span class="day"><?php echo esc_html( date( 'j', $date ) ); ?></span>
+									</div>
+
+									<h3 class="time">
+										<?php echo esc_html( E3_Challenge::get_field( 'start_time' ) ); ?> - <?php echo esc_html( E3_Challenge::get_field( 'end_time' ) ); ?>
+									</h3>
+
+									<h2><?php the_title(); ?></h2>
+
+									<a href="<?php the_permalink(); ?>">Learn More</a>
+								</div>
+								<?php
+							}
+						?>
+					</div>
+				</div>
+			</div><!-- .entry-content -->
+
+		</article><!-- #post-## -->
+		<?php
+		endwhile;
+		// End of the loop.
+		?>
+
+	</main><!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
